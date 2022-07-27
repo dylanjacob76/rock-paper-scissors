448315e013d826a8a8bfbe9079ef4fbef40f9067
dylanjacob76 <business.dmjacob@gmail.com>
2022-07-27 10:16:54 -0400

    finished the JavaScript code

---
+++ assets/js/script.js
@@ -0,0 +1,55 @@
+var ties = 0;
+var wins = 0;
+var losses = 0;
+
+var rps = ["R", "P", "S"];
+
+function startGame() {
+    var userChoice = this.prompt("Enter R, P, or S:");
+
+    if (!userChoice) {
+        alert("Refresh the page if you change your mind!");
+    }
+
+    userChoice = userChoice.toUpperCase();
+
+    var computerChoice = rps[Math.floor(rps.length * Math.random())];
+
+    if (userChoice === computerChoice) {
+        alert("The computer chose " + computerChoice + ". It's a TIE!");
+        ties++;
+    } else if (userChoice === "R" && computerChoice === "S") {
+        alert("The computer chose " + computerChoice + ". You WON!")
+        wins++;
+    } else if (userChoice === "R" && computerChoice === "P") {
+        alert("The computer chose " + computerChoice + ". You LOST!")
+        losses++;
+    } else if (userChoice === "P" && computerChoice === "R") {
+        alert("The computer chose " + computerChoice + ". You WON!")
+        wins++;
+    } else if (userChoice === "P" && computerChoice === "S") {
+        alert("The computer chose " + computerChoice + ". You LOST!")
+        losses++;
+    } else if (userChoice === "S" && computerChoice === "P") {
+        alert("The computer chose " + computerChoice + ". You WON!")
+        wins++;
+    } else if (userChoice === "S" && computerChoice === "R") {
+        alert("The computer chose " + computerChoice + ". You LOST!")
+        losses++;
+    } else {
+        alert("Error. Please refreshthe page!")
+    }
+
+    this.alert("\nUser Stats" + "\n\nWins: "+ wins + "\nLosses: " + losses + "\nTies: " + ties);
+
+    var playAgain = confirm("Would you like to play again?");
+
+    if (playAgain) {
+        startGame();
+    } else {
+        alert("Refresh the page if you change your mind!");
+    }
+    
+}
+
+startGame();
